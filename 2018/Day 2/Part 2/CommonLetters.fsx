12b0a60d77af365a9a3b0b6f521780e197701a6b
Timothé LARIVIERE <timothe.lariviere@outlook.com>
2018-12-05 23:58:56 +0100

    More functional solution for Day 2 / Part 2

--- 2018/Day 2/Part 2/CommonLetters.fsx
+++ 2018/Day 2/Part 2/CommonLetters.fsx
@@ -1,12 +1,14 @@
 System.IO.File.ReadAllLines "2018/Day 2/data.txt"
-|> (fun lines ->
-    let mutable foundChars = ""
+|> (fun lines -> seq {
     for i = 0 to (lines.Length - 2) do
         for j = i to (lines.Length - 1) do
-            if foundChars = "" then
-                let map = lines.[i] |> String.mapi (fun index c -> if c = lines.[j].[index] then c else ' ')
-                let errors = map |> String.filter (fun c -> c = ' ') |> String.length
-                if errors = 1 then
-                    foundChars <- map |> String.filter (fun c -> c <> ' ')
-    foundChars
+            let convert = (lines.[i] |> String.mapi (fun index c -> if c = lines.[j].[index] then c else ' '))
+            yield convert })
+|> Seq.tryFind (fun str ->
+    match str |> String.filter (fun c -> c = ' ') |> String.length with
+    | 1 -> true
+    | _ -> false
 )
+|> function
+   | Some str -> str |> String.filter (fun c -> c <> ' ')
+   | None -> ""
